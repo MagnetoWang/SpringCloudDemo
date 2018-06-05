d8e7aff074e3c7368274bdd65086273a4135ac0e
MagnetoWang <1527672216@qq.com>
2018-06-05 14:44:41 +0800

    使用了公司的pom和设置以后。所有jar的依赖冲突和版本不一致都得到了解决。显然一个稳定的仓库和设置是为后续写代码的保障

--- cloudserver/src/main/java/com/magnetowang/cloudserver/CloudserverApplication.java
+++ cloudserver/src/main/java/com/magnetowang/cloudserver/CloudserverApplication.java
@@ -3,7 +3,6 @@ package com.magnetowang.cloudserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication

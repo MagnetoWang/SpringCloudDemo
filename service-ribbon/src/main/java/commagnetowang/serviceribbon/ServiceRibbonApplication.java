73c2552dcf54e55cba10124681d54aca3625661c
MagnetoWang <1527672216@qq.com>
2018-06-05 15:46:29 +0800

    完成ribbon仪表盘的操作。不能完全看教程。自己也要推理思考一下

--- service-ribbon/src/main/java/commagnetowang/serviceribbon/ServiceRibbonApplication.java
+++ service-ribbon/src/main/java/commagnetowang/serviceribbon/ServiceRibbonApplication.java
@@ -2,9 +2,11 @@ package commagnetowang.serviceribbon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.web.client.RestTemplate;
@@ -12,7 +14,8 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
-
+@EnableHystrixDashboard
+@EnableCircuitBreaker
 public class ServiceRibbonApplication {
 
     public static void main(String[] args) {

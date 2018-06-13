4baee2ea9fcdc7b9f8735df0097ab1edbcf59025
MagnetoWang <1527672216@qq.com>
2018-06-13 15:35:51 +0800

    消息总线的配置。基本功能可以自动化更新服务器的相关配置或者服务器的内容。注意问题就是权限，用户名和密码

--- config-client/src/main/java/com/magnetowang/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/magnetowang/configclient/ConfigClientApplication.java
@@ -3,13 +3,16 @@ package com.magnetowang.configclient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-
+@EnableEurekaClient
+@RefreshScope
 @PropertySource(value={"classpath:bootstrap.properties"})
 public class ConfigClientApplication {
 
